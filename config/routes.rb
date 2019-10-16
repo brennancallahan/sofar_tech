d3e25457f75d3d02f99049b6954f01e6473eebb0
Brennan Callahan <brennancallahan@Brennans-MacBook-Pro-2.local>
2019-10-16 08:56:50 -0400

    cleaned up routes, added logic on view to recreate 'back' experience

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :videos, only: [:index]
-  get '/' => 'videos#index', :as => 'search_page_path'
+  get '/' => 'videos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
